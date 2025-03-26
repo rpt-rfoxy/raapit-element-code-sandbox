316c186fae822aaa271c55aa0139cc05ea2b782d
foxy <foxy@foxy.com>
2025-03-27 06:50:11 +0700

    add react-hook-form

--- src/date-time-input/date-time-input.tsx
+++ src/date-time-input/date-time-input.tsx
@@ -1,4 +1,5 @@
-import React, { forwardRef, LegacyRef } from 'react'
+import React, { forwardRef } from 'react'
+import { useFormContext } from 'react-hook-form'
 import {
   ElInputField,
   ElInputSizesEnum,
@@ -8,22 +9,25 @@ import { LabelText } from '../label-text'
 import { ErrorText } from '../error-text'
 
 export interface DateTimeInputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
-  isError?: boolean
+  name: string
   size?: ElInputSizesEnum
   label?: string
   isRequired?: boolean
-  errorMessage?: string
   helperText?: string
   type?: 'time' | 'date' | 'datetime-local' | 'week' | 'month'
 }
 
-export const DateTimeInput: React.ForwardRefExoticComponent<DateTimeInputProps> = forwardRef(
-  (
-    { isError, isRequired, label, prefix, size, errorMessage, helperText, type = 'date', ...rest },
-    ref: React.ForwardedRef<React.InputHTMLAttributes<HTMLInputElement>>,
-  ) => {
+export const DateTimeInput = forwardRef<HTMLInputElement, DateTimeInputProps>(
+  ({ name, isRequired, label, size, helperText, type = 'date', ...rest }, ref) => {
+    const {
+      register,
+      formState: { errors },
+    } = useFormContext()
+
     const textSize: ElInputSizesEnum = size == 'large' ? 'medium' : 'small'
     const placeholder: string | undefined = isRequired && !label ? `${rest.placeholder} *` : rest.placeholder
+    const errorMessage = errors[name]?.message as string | undefined
+
     return (
       <ElDateTimeInput>
         {label && (
@@ -33,17 +37,20 @@ export const DateTimeInput: React.ForwardRefExoticComponent<DateTimeInputProps> 
         )}
         <ElInputField
           {...rest}
+          {...register(name, { required: isRequired ? 'This field is required' : false })}
           type={type}
-          data-is-error={isError}
+          data-is-error={!!errorMessage}
           data-size={size}
           required={isRequired}
           placeholder={placeholder}
           aria-label={`Input type ${type}`}
-          ref={ref as unknown as LegacyRef<HTMLInputElement>}
+          ref={ref}
         />
-        <ErrorText isError={isError} data-size={textSize}>{errorMessage}</ErrorText>
-        {!isError && helperText && <LabelText size={textSize}>{helperText}</LabelText>}
+        <ErrorText isError={!!errorMessage} data-size={textSize}>
+          {errorMessage}
+        </ErrorText>
+        {!errorMessage && helperText && <LabelText size={textSize}>{helperText}</LabelText>}
       </ElDateTimeInput>
     )
-  },
+  }
 )
